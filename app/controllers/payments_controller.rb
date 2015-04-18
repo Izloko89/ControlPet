6f769f2bed547d1b18ba2b7a3052696141a31cde
Avillegas <alexiseliudrdz@gmail.com>
2015-04-17 20:07:38 -0500

    se corrige el reporte de pagos tab today

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -2,7 +2,7 @@ class PaymentsController < ApplicationController
 
 	def make_payment
 		@sale = Sale.find(params[:payments][:sale_id])
-		payment = Payment.create(:payment_type => params[:payments][:payment_type], :amount => params[:payments][:amount], :sale_id => params[:payments][:sale_id])
+		payment = Payment.create(:payment_type => params[:payments][:payment_type], :amount => params[:payments][:amount], :sale_id => params[:payments][:sale_id],:user_id => current_user.user_id)
 
   	respond_to do |format|
       format.js
