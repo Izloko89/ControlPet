a0ecb4699940279cbe38152591f4f75e051e5f57
Avillegas <alexiseliudrdz@gmail.com>
2015-05-04 19:09:34 -0500

    deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,22 @@ PushvendorPos::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Do not dump schema after migrations.
-  config.assets.initialize_on_precompile = false
+  config.assets.initialize_on_precompile = true
+
+  config.assets.precompile << Proc.new do |path|
+  if path =~ /\.(css|js)\z/
+    full_path = Rails.application.assets.resolve(path).to_path
+    app_assets_path = Rails.root.join('app', 'assets').to_path
+    if full_path.starts_with? app_assets_path
+      puts "including asset: " + full_path
+      true
+    else
+      puts "excluding asset: " + full_path
+      false
+    end
+  else
+    false
+  end
+end
   
 end
