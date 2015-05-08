4a800de8aeb7dde01c0c8fa1cb61da2d226a0185
archagy <alexiseliudrdz@gmail.com>
2015-05-08 13:11:42 -0500

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,22 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+
+config.assets.precompile << Proc.new { |path|
+  if path =~ /\.(css|js)\z/
+    full_path = Rails.application.assets.resolve(path).to_path
+    app_assets_path = Rails.root.join('app', 'assets').to_path
+    if full_path.starts_with? app_assets_path
+      puts "including asset: " + full_path
+      true
+    else
+      puts "excluding asset: " + full_path
+      false
+    end
+  else
+    false
+  end
+}
   # Code is not reloaded between requests.
   config.cache_classes = true
 
