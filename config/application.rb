d481ad86f5ef5314db9f91a25944912bd81bb9b6
archagy <alexiseliudrdz@gmail.com>
2015-05-08 13:28:28 -0500

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,7 @@ module ControlPet
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.assets.initialize_on_precompile = false 
+    config.assets.initialize_on_precompile = false
+    config.assets.precompile = ['*.js', '*.css', '**/*.js', '**/*.css']
   end
 end
